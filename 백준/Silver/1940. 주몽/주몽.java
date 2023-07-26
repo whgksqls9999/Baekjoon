e4df8ffdce91284c164c31e22f8577db7dc78f72
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-07-27 01:46:17 +0900

    [Silver IV] Title: 주몽, Time: 140 ms, Memory: 15044 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int[] a = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N;i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int start = 0;
+		int end = a.length-1;
+		int sum;
+		int cnt = 0;
+		Arrays.sort(a);
+		
+		while (start < end) {
+			sum = a[start] + a[end];
+			if (sum == M) {
+				cnt++;
+				start++;
+				end--;
+			} else if (sum > M) {
+				end--;
+			} else {
+				start++;
+			}
+		}
+		
+
+
+		System.out.println(cnt);
+	}
+}
