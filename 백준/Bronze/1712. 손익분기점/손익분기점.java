f36d0133b528decf7e043c654171ce5f02cc50a9
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-07-30 00:53:26 +0900

    [Bronze II] Title: 손익분기점, Time: 1424 ms, Memory: 11864 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Integer.parseInt(st.nextToken());
+		long B = Integer.parseInt(st.nextToken());
+		long C = Integer.parseInt(st.nextToken());
+		long i = 0;
+		
+		if(B >= C) {
+			bw.write(""+-1);
+			bw.flush();
+			return;
+		}
+
+		
+		while(true) {
+			if (sell(C,i) > use(A,B,i)) {
+				bw.write(""+i);
+				bw.flush();
+				return;
+			}
+			i++;
+		}
+	}
+	static long use(long A, long B, long i) {
+		return A + B * i;
+	}
+	static long sell(long C, long i) {
+		return C * i;
+	}
+}
