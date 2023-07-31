fbca538d27faa939cbaba2404d1b9e687bcb8f72
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-08-01 01:58:19 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 13232 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for (int i = 0; i < N-1; i++) {
+			for(int j = 0; j < N-i-1; j++) {
+				if (arr[j] > arr[j+1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+		}
+		for (int i : arr) {
+			sb.append(i+"\n");
+		}
+		System.out.println(sb);
+	}
+}
