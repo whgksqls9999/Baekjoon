dd9e42a1848f6e9fefdcbb438cc4999e0fe1ca27
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-08-10 22:26:55 +0900

    [Silver I] Title: 스타트링크, Time: 204 ms, Memory: 55076 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -40,16 +40,16 @@ public class Main {
 			if (now + U <= F && (visited[now + U] == 0)) {
 				visited[now + U] = visited[now] + 1;
 				queue.add(now + U);
-			}
+			} // 엘리베이터 UP 버튼. 방문한 적이 없고, 최고층보다 작거나 같다면 들어간다.
 			if (now - D >= 1 && (visited[now - D] == 0)) {
 				visited[now - D] = visited[now] + 1;
 				queue.add(now - D);
-			}
+			} // 엘리베이터 DOWN 버튼. 방문한 적이 없고, 최하층보다 크거나 같다면 들어간다.
 		}
-		if (visited[end] - 1 == -1) {
+		if (visited[end] - 1 == -1) { // 목적지에 도착하지 못했을 때
 			System.out.println("use the stairs");
-		} else {
-			System.out.println(visited[end] - 1);
+		} else { // 목적지에 도착했을 때
+			System.out.println(visited[end] - 1); // 처음 시작할때 가지고 있던 1은 빼준다.
 		}
 
 	}
