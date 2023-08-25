7b3981c01d0684727ff6631ce80cc950ab0b969d
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-08-26 00:13:48 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 84 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static long[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(st.nextToken());
+		dp = new long[n + 1];
+
+		for (int i = 2; i < dp.length; i++) {
+			dp[i] = -1;
+		}
+		dp[0] = 0;
+		if (n > 1) {
+			dp[2] = 1;
+		}
+		if (n > 0) {
+			dp[1] = 1;
+		}
+		System.out.println(fibo(n));
+
+	} // main
+
+	static long fibo(int num) {
+		if (dp[num] != -1) {
+			return dp[num];
+		}
+		return dp[num] = fibo(num - 1) + fibo(num - 2);
+	}
+}
