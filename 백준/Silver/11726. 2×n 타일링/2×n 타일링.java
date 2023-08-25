d01ea8af4b78e53ca288a85dfa93f2344f3c5f0e
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-08-25 23:24:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 80 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(st.nextToken());
+
+		// 점화식
+		// N의 경우 = N-1의 경우 + N-2의 경우
+		dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int i = 2; i < dp.length; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		}
+
+		System.out.println(dp[n]);
+	} // main
+
+}
