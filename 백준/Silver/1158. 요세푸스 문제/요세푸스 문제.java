9683ef87fc55f9a113864190fbd87f830d01dd15
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-09-24 01:46:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 516 ms, Memory: 292284 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(st.nextToken()); // N명
+		int K = Integer.parseInt(st.nextToken()); // K번째 사람 제거
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 0; i < N; i++) {
+			queue.add(i + 1);
+		}
+
+		sb.append("<");
+		while (queue.size() != 1) {
+			for (int i = 0; i < K - 1; i++) {
+				queue.add(queue.poll());
+			}
+			sb.append(queue.poll()).append(", ");
+		}
+		sb.append(queue.poll()).append(">");
+		System.out.println(sb);
+	}
+}
