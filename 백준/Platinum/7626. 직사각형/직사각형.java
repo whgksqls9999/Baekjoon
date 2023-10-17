0d03daf9fde1c28c82263ca029bb5105ccb61c83
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-10-17 23:21:04 +0900

    [Platinum I] Title: 직사각형, Time: 2480 ms, Memory: 215844 KB -BaekjoonHub

--- 백준/Platinum/7626. 직사각형/직사각형.java
+++ 백준/Platinum/7626. 직사각형/직사각형.java
@@ -28,23 +28,23 @@ public class Main {
 		// pos : {x좌표, y좌표, 높이} 를 담아주는 List
 		pos = new ArrayList<>();
 
-		// 제시된 두 점의 높이를 통일시키고, 높이차를 계산하여 List에 배열 형태로 넣기
+		// 좌표를 받아 List에 2차원 배열 형태로 저장하기
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken(" "));
 			int x2 = Integer.parseInt(st.nextToken(" "));
 			int y1 = Integer.parseInt(st.nextToken(" "));
 			int y2 = Integer.parseInt(st.nextToken(" "));
-//			int height = y2 - y1;
 
-			pos.add(new int[] { x1, y1, y2 });
-			pos.add(new int[] { x2, y2, y1 });
-
+			// y2 - y1 : 해당 x좌표에서 직사각형의 높이
+			pos.add(new int[] { x1, y1, y2 }); // [1] : y1 / [2] : y2 => 직사각형의 시작지점
+			pos.add(new int[] { x2, y2, y1 }); // [1] : y2 / [2] : y1 => 직사각형의 종료지점
 		}
 
 		// comp : 좌표 압축을 위한 HashMap
 		comp = new HashMap<>();
 
+		// compOriginArr : 압축 이전의 원래 좌표를 넣을 HashMap
 		compOriginArr = new int[pos.size()];
 
 		// y축 정렬
@@ -64,8 +64,7 @@ public class Main {
 				comp.put(cur[1], ++idx);
 			}
 			compOriginArr[idx] = cur[1];
-//			compOriginY.put(idx, cur[1]);
-
+			
 			cur[1] = comp.get(cur[1]);
 		}
 
@@ -77,12 +76,12 @@ public class Main {
 			}
 		});
 
-
-		int h = (int) Math.ceil(Math.log(comp.size() - 1) / Math.log(2));
-		int size = 1_600_000;
+//		int h = (int) Math.ceil(Math.log(comp.size() - 1) / Math.log(2));
+//		int size = 1_600_000;
+		int h = (int) Math.ceil(Math.log(N*2) / Math.log(2));
+		int size = 1 << h+1;
 
 		tree = new long[size][2];
-
 
 		long res = 0;
 		int[] pre = new int[] { 0, 0, 0 };
@@ -107,25 +106,50 @@ public class Main {
 	}
 
 	static void update(int node, int start, int end, int left, int right, int val) {
-		int mid = (start + end) / 2;
-
-		if (left <= start && end <= right) {
-			tree[node][1] += val;
-		} else {
-			if (left <= mid)
-				update(node * 2, start, mid, left, right, val);
-			if (right > mid)
-				update(node * 2 + 1, mid + 1, end, left, right, val);
+		// 찾고자 하는 범위를 벗어났다면 리턴
+		if (end < left || right < start) {
+			return;
 		}
 
+		// 중간 점 구하기
+		int mid = (start + end) / 2;
+
+		// 해당 구간 범위에 들어왔다면
+		if (left <= start && end <= right) {
+
+			// 해당 구간의 세로 선을 활성화시킨다.
+			tree[node][1] += val;
+		}
+
+		// 범위에서 벗어났다면
+		else {
+			update(node * 2, start, mid, left, right, val);
+			update(node * 2 + 1, mid + 1, end, left, right, val);
+		}
+
+		// 해당 구간에 세로 선이 하나 이상 활성화 됐다면
 		if (tree[node][1] != 0) {
+
+			// 해당 구간의 세로 길이를 저장한다.
 			tree[node][0] = compOriginArr[end + 1] - compOriginArr[start];
-		} else {
-			if (start == end)
+		}
+
+		// 해당 구간에 선이 모두 죽어있다면
+		else {
+
+			// 리프 노드일 때만
+			if (start == end) {
+
+				// 해당 구간의 선을 비활성화 시킨다(값을 초기화시킨다)
 				tree[node][0] = 0;
-			else
+
+			} else {
+
+				// 리프노드가 아니라면 자손 노드의 값들을 더해준다.
 				tree[node][0] = tree[node * 2][0] + tree[node * 2 + 1][0];
+			}
 		}
 
 	}
+
 }
