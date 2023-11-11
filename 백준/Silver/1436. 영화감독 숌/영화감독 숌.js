e91b25ec32e5746a7a94f5fea0a4cbff9dce2865
whgksqls9999 <85666312+whgksqls9999@users.noreply.github.com>
2023-11-12 03:16:26 +0900

    [Silver V] Title: 영화감독 숌, Time: 1084 ms, Memory: 27652 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+input = parseInt(input);
+
+let cnt = 0;
+let i = 666;
+for (i = 666; cnt != input; ++i){
+    if(i.toString(10).match('666')){
+        ++cnt;
+    }
+}
+console.log(i-1);
