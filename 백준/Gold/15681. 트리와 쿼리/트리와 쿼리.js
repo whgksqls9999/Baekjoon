96d2d53ac11f1bcc923370bc300e36cdeaa03bc1
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2023-12-04 14:48:45 +0900

    [Gold V] Title: 트리와 쿼리, Time: 856 ms, Memory: 124832 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.js
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.js
@@ -1,55 +1,50 @@
-// let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-// 이건 백준에서 제출시 인풋 받기
-
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
 const info = input[0].split(" ").map((element) => Number(element));
 
 let [N, R, Q] = info;
 
+// 트리 연결 정보 저장하기
 let nodes = Array(N + 1)
   .fill(0)
   .map(() => Array(0));
-
 for (let i = 1; i <= N - 1; ++i) {
   let conn = input[i].split(" ").map((element) => Number(element));
-
   nodes[conn[0]].push(conn[1]);
   nodes[conn[1]].push(conn[0]);
 }
 
+// 트리를 이루는 노드의 깊이 구하기
 let visited = Array(N + 1).fill(false);
-let cnt;
-let answer = [];
-let dp = Array(N + 1).fill(0);
 let depth = Array(N + 1).fill(0);
+DFStoCalDepth(R, 1);
 
-DFStoDepth(R, 1);
-
-visited = Array(N + 1).fill(false);
+// 구해진 깊이를 사용해서 각 정점에서의 서브트리 노드 개수 카운트(dp)
+let dp = Array(N + 1).fill(0);
+let answer = [];
 DFS(R);
 
+// 각 쿼리마다 정답 출력하기
 for (let i = 0; i < Q; ++i) {
   let query = Number(input[N + i]);
-
   answer.push(dp[query]);
 }
 console.log(answer.join("\n"));
 
-function DFStoDepth(node, depthCnt) {
+// functions
+function DFStoCalDepth(node, depthCnt) {
   visited[node] = true;
   depth[node] = depthCnt;
 
   nodes[node].forEach((element) => {
     if (!visited[element]) {
-      DFStoDepth(element, depthCnt + 1);
+      DFStoCalDepth(element, depthCnt + 1);
     }
   });
 }
 
 function DFS(node) {
   visited[node] = true;
-  ++cnt;
   if (dp[node] === 0) {
     dp[node]++;
   }
