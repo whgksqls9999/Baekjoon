85a8828f5938bdf9e4f9585e0e5ff0d25fc628a1
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2023-12-18 12:09:16 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.js
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.js
@@ -5,25 +5,24 @@ const input = require("fs")
   .split("\n");
 
 // 초기 설정
-input.pop();
 
 (function solution() {
   const answer = [];
-  for (let i = 0; i < input.length; ++i) {
+  for (let i = 0; i < input.length - 1; ++i) {
     const cur = input[i].split(" ").map((element) => Number(element));
 
-    // let sum = cur.reduce((acc, cur) => acc + cur, 0);
-    // let max = Math.max(...cur);
-    // if (sum - max <= max) {
-    //   answer.push("Invalid");
-    //   continue;
-    // }
-
-    cur.sort((a, b) => a - b);
-    if (cur[0] + cur[1] <= cur[2]) {
+    let sum = cur.reduce((acc, cur) => acc + cur, 0);
+    let max = Math.max(...cur);
+    if (sum - max <= max) {
       answer.push("Invalid");
       continue;
     }
+
+    // cur.sort((a, b) => a - b);
+    // if (cur[0] + cur[1] <= cur[2]) {
+    //   answer.push("Invalid");
+    //   continue;
+    // }
 
     const set = new Set(cur);
     if (set.size === 1) {
