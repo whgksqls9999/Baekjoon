ede5270c8666e109ac9b70c2b5771a6efead06a9
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2023-12-18 12:08:38 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.js
@@ -0,0 +1,38 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+// 초기 설정
+input.pop();
+
+(function solution() {
+  const answer = [];
+  for (let i = 0; i < input.length; ++i) {
+    const cur = input[i].split(" ").map((element) => Number(element));
+
+    // let sum = cur.reduce((acc, cur) => acc + cur, 0);
+    // let max = Math.max(...cur);
+    // if (sum - max <= max) {
+    //   answer.push("Invalid");
+    //   continue;
+    // }
+
+    cur.sort((a, b) => a - b);
+    if (cur[0] + cur[1] <= cur[2]) {
+      answer.push("Invalid");
+      continue;
+    }
+
+    const set = new Set(cur);
+    if (set.size === 1) {
+      answer.push("Equilateral");
+    } else if (set.size === 2) {
+      answer.push("Isosceles");
+    } else {
+      answer.push("Scalene");
+    }
+  }
+  console.log(answer.join("\n"));
+})();
