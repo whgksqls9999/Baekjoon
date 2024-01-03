aa7f97b296ea96e33270706b195f469127d8ef06
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-01-03 17:26:51 +0900

    [Silver V] Title: 타일 장식물, Time: 116 ms, Memory: 9324 KB, Score: 21 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString();
+
+const N = Number(input[0]);
+const dp = Array(N+1).fill(0);
+
+dp[0] = BigInt(0);
+dp[1] = BigInt(1);
+dp[2] = BigInt(1);
+
+(function solution(){
+    for (let i = 3; i <= N; ++i){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    console.log((dp[N] * BigInt(2) + (dp[N] + dp[N-1])*BigInt(2)).toString());
+}());
