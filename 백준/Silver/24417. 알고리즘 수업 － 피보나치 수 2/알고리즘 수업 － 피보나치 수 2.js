aaa1c4fa7b6cd12277d03b0af8b15853d7fc8521
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-01-07 02:27:18 +0900

    [Silver IV] Title: 알고리즘 수업 - 피보나치 수 2, Time: 1896 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.js
+++ 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.js
@@ -4,16 +4,19 @@ const input = require("fs")
   .trim()
   .split("\n");
 const N = Number(input[0]);
-const dp = Array(2).fill(0);
-dp[0] = 1;
-dp[1] = 1;
+// const dp = Array(2).fill(0);
+// dp[0] = 1;
+// dp[1] = 1;
+let A = 1;
+let B = 1;
 const MOD = 1_000_000_007;
 
 (function solution() {
-  let cur;
   for (let i = 3; i <= N; ++i) {
-    dp[i % 2] = (dp[0] + dp[1]) % MOD;
-    cur = i % 2;
+    let tmp = A + B;
+    if (tmp >= MOD) tmp %= MOD;
+    A = B;
+    B = tmp;
   }
-  console.log(`${dp[cur]} ${(N - 2) % MOD}`);
+  console.log(`${B} ${(N - 2) % MOD}`);
 })();
