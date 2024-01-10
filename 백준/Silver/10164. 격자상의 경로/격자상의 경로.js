ec2cb63dde7c96f8a9da01a01538e9223a0f47eb
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-01-10 12:19:22 +0900

    [Silver I] Title: 격자상의 경로, Time: 148 ms, Memory: 9600 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.js
@@ -0,0 +1,36 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+(function solution() {
+  const [N, M, K] = input[0].split(" ").map(Number);
+  const dp = Array(N + 1)
+    .fill(0)
+    .map(() => Array(M + 1).fill(0));
+
+  let r = Math.floor(K / M) + 1;
+  let c = K % M;
+
+  if (K === 0) {
+    [r, c] = [N, M];
+  }
+
+  for (let i = 1; i <= r; ++i) {
+    for (let j = 1; j <= c; ++j) {
+      if (i === 1 && j === 1) {
+        dp[i][j] = 1;
+        continue;
+      }
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  for (let i = r; i <= N; ++i) {
+    for (let j = c; j <= M; ++j) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  console.log(dp[N][M]);
+})();
