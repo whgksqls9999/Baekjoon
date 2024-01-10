cb80ecd23b4b23ef881074041ffe40cec7da2b71
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-01-11 00:21:02 +0900

    [Bronze V] Title: 삼각형, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+
+console.log(((N * M) / 2).toFixed(1));
