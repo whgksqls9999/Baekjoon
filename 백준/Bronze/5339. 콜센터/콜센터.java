ea2e0a7acdc548f4532deb52655b6e2bdee23980
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-03-02 10:57:11 +0900

    [Bronze V] Title: 콜센터, Time: 68 ms, Memory: 11272 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String a = "     /~\\\n"
+				+ "    ( oo|\n"
+				+ "    _\\=/_\n"
+				+ "   /  _  \\\n"
+				+ "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n"
+				+ "============\n"
+				+ "|          |\n"
+				+ "|          |\n"
+				+ "|          |";
+		System.out.println(a);
+	}
+}
