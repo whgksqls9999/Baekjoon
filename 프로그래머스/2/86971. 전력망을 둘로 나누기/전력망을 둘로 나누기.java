ea7f5b2f59ed081f722de7d29b2ff134c4dfa5d9
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-05-27 16:59:51 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.08 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         
+        // 인접 리스트
         List<Integer>[] nodes = new ArrayList[n + 1];
         for (int i = 1; i < nodes.length; i++){
             nodes[i] = new ArrayList<>();
@@ -14,7 +15,9 @@ class Solution {
             nodes[wire[1]].add(wire[0]);
         }
         
+        
         for (int[] wire : wires){
+            // 와이어 하나 골라서 연결 끊기
             for (int i = 0; i < nodes[wire[0]].size(); i++){
                 if (nodes[wire[0]].get(i) == wire[1]){
                     nodes[wire[0]].remove(i);
@@ -29,6 +32,7 @@ class Solution {
                 }
             }
             
+            // 끊어진 와이어 환경에서 BFS 돌리기
             boolean[] visited = new boolean[n + 1];
             
             List<Integer> list = new ArrayList<>();
@@ -38,6 +42,7 @@ class Solution {
                 list.add(BFS(i, visited, nodes));
             }
             
+            // 끊어진 와이어 다시 복구시키기
             nodes[wire[0]].add(wire[1]);
             nodes[wire[1]].add(wire[0]);
             
