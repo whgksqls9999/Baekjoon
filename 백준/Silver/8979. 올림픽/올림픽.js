bcabd0bb966c7f5fe72947f78a8ec65c89f45737
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-07-02 21:07:10 +0900

    [Silver V] Title: 올림픽, Time: 208 ms, Memory: 10144 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.js
+++ 백준/Silver/8979. 올림픽/올림픽.js
@@ -6,35 +6,49 @@ const input = require('fs')
 
 const countries = [];
 
+function compare(a, b) {
+	for (let i = 0; i < a.medal.length; i++) {
+		let aCur = +a.medal[i];
+		let bCur = +b.medal[i];
+
+		if (aCur > bCur) {
+			return -1;
+		} else if (aCur < bCur) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+function returnAnswer(arr, N) {
+	for (let cur of arr) {
+		if (cur.num === N) {
+			return cur.rank;
+		}
+	}
+}
 const N = +input[0].split(' ')[1];
 
 for (let i = 1; i < input.length; i++) {
 	const cur = input[i].trim().split(' ');
 	countries.push({
 		num: +cur[0],
-		medal: cur.slice(1, cur.length).join(' '),
-		toString() {
-			return this.medal;
-		},
+		medal: cur.slice(1, cur.length),
 	});
 }
 
-countries.sort((a, b) => b.toString().localeCompare(a.toString()));
-
+countries.sort((a, b) => compare(a, b));
 countries[0].rank = 1;
 
 for (let i = 1; i < countries.length; i++) {
 	let cur = countries[i];
 
-	if (cur.toString() == countries[i - 1].toString()) {
+	if (!compare(cur, countries[i - 1])) {
 		cur.rank = countries[i - 1].rank;
 	} else {
 		cur.rank = i + 1;
 	}
 }
 
-for (let i = 0; i < countries.length; i++) {
-	if (countries[i].num === N) {
-		return console.log(countries[i].rank);
-	}
-}
+console.log(returnAnswer(countries, N));
