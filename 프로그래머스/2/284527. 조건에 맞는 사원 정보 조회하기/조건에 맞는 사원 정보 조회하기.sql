ef331eda82ebc6f58f3c85ce75e02abd5201e2f1
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-07-04 22:01:34 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,23 @@
+SELECT
+    SUM(G.SCORE) AS SCORE,
+    E.EMP_NO, 
+    E.EMP_NAME, 
+    E.POSITION, 
+    E.EMAIL
+FROM
+    HR_EMPLOYEES E
+JOIN
+    HR_GRADE G
+ON
+    E.EMP_NO = G.EMP_NO
+GROUP BY
+    G.EMP_NO,
+    G.YEAR
+ORDER BY
+    SCORE DESC
+LIMIT
+    1
+
+    
+
+    
