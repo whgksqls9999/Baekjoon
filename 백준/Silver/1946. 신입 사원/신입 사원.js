4aa0d7a5de6858d92aeb1ef4d5c287c7e572789d
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-07-05 01:31:30 +0900

    [Silver I] Title: 신입 사원, Time: 4236 ms, Memory: 410972 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -0,0 +1,51 @@
+const input = require('fs')
+	.readFileSync('/dev/stdin')
+	.toString()
+	.trim()
+	.split('\n');
+
+let I = 0;
+let T = +input[I++];
+
+function solution() {
+	let answer = '';
+
+	while (T-- > 0) {
+		let N = +input[I++];
+		const arr = [];
+		let a = (b = Number.MAX_VALUE);
+
+		for (let i = 0; i < N; i++) {
+			let cur = input[I++].split(' ').map(Number);
+			arr.push(cur);
+
+			a = Math.min(a, cur[0]);
+			b = Math.min(b, cur[1]);
+		}
+		arr.sort((a, b) => a[0] - b[0]);
+		let resultA = 1;
+		let min = arr[0][1];
+		for (let i = 1; i < arr.length; i++) {
+			if (arr[i][1] < min) {
+				resultA++;
+			}
+			min = Math.min(min, arr[i][1]);
+		}
+
+		arr.sort((a, b) => a[1] - b[1]);
+
+		let resultB = 1;
+		min = arr[0][0];
+		for (let i = 1; i < arr.length; i++) {
+			if (arr[i][0] < min) {
+				resultB++;
+			}
+			min = Math.min(min, arr[i][1]);
+		}
+
+		answer += Math.max(resultA, resultB) + '\n';
+	}
+	console.log(answer.trim());
+}
+
+solution();
