865de0379baab76cd4776d65b47b52eea29006bf
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-07-05 23:18:16 +0900

    [Gold III] Title: 두 배열의 합, Time: 808 ms, Memory: 98300 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.js
@@ -0,0 +1,53 @@
+const input = require('fs')
+	.readFileSync('/dev/stdin')
+	.toString()
+	.trim()
+	.split('\n');
+
+let idx = 0;
+let T = +input[idx++];
+
+function solution() {
+	let answer = 0;
+
+	idx++;
+	const A = [0].concat(input[idx++].split(' ').map(Number));
+	idx++;
+	const B = [0].concat(input[idx++].split(' ').map(Number));
+
+	for (let i = 1; i < A.length; i++) {
+		A[i] = A[i - 1] + A[i];
+	}
+
+	for (let i = 1; i < B.length; i++) {
+		B[i] = B[i - 1] + B[i];
+	}
+
+	const mapA = {};
+	for (let i = 0; i < A.length - 1; i++) {
+		for (let j = i + 1; j < A.length; j++) {
+			let cur = A[j] - A[i];
+			mapA[cur] = (mapA[cur] ?? 0) + 1;
+		}
+	}
+
+	const mapB = {};
+	for (let i = 0; i < B.length - 1; i++) {
+		for (let j = i + 1; j < B.length; j++) {
+			let cur = B[j] - B[i];
+
+			if (!mapA[T - cur]) continue;
+			mapB[cur] = (mapB[cur] ?? 0) + 1;
+		}
+	}
+
+	for (let key in mapA) {
+		if (!mapB[T - +key]) continue;
+
+		answer += mapA[+key] * mapB[T - +key];
+	}
+
+	console.log(answer);
+}
+
+solution();
