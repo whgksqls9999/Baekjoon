d87fcf97fbb6700ff0b430e1c41044c29e8ee985
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-07-07 16:35:19 +0900

    [Silver I] Title: IOIOI, Time: 196 ms, Memory: 25876 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -6,23 +6,35 @@ const input = require('fs')
 
 let idx = 0;
 const N = +input[idx++];
-const word = 'I' + 'OI'.repeat(N);
+const word = 'IOI';
 idx++;
 const str = input[idx++].trim();
 
 function solution() {
 	let answer = 0;
 
-	let l = 0;
-	let r = word.length;
-	while (r <= str.length) {
-		let cur = str.slice(l++, r++);
-
-		if (cur === word) {
-			answer++;
+	let check = 1;
+	let pos = 0;
+	let result = [];
+	while (check !== -1) {
+		check = str.indexOf(word, pos);
+		result.push(check);
+		pos = check + 2;
+	}
+	pos = 0;
+	let result2 = [1];
+	for (let i = 1; i < result.length - 1; i++) {
+		if (result[i] - result[i - 1] === 2) {
+			result2[pos]++;
+		} else {
+			result2[++pos] = 1;
 		}
 	}
 
+	for (let key of result2) {
+		let cur = key - N + 1;
+		answer += cur > 0 ? cur : 0;
+	}
 	console.log(answer);
 }
 
