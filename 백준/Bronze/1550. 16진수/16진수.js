dc818de830a738ac837b4749b1cd96be9bc68800
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2024-11-12 22:09:36 +0900

    [Bronze II] Title: 16진수, Time: 108 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.js
@@ -0,0 +1,29 @@
+const input = require('fs')
+	.readFileSync('/dev/stdin')
+	.toString()
+	.trim()
+	.split('\n');
+
+function solution() {
+	let idx = 0;
+	let answer = 0;
+	const N = input[idx++].trim();
+
+	const trans = {
+		A: 10,
+		B: 11,
+		C: 12,
+		D: 13,
+		E: 14,
+		F: 15,
+	};
+
+	for (let i = 0; i < N.length; i++) {
+		let cur = trans[N[i]] ?? N[i];
+		answer += Math.pow(16, N.length - i - 1) * cur;
+	}
+
+	return answer;
+}
+
+console.log(solution());
