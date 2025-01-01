9fbc05204b86718107eb7ab67882a5404d48ab04
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2025-01-01 23:41:31 +0900

    [Silver IV] Title: 마법의 돌 장난감, Time: 96 ms, Memory: 9400 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21554. 마법의 돌 장난감/마법의 돌 장난감.js
+++ 백준/Silver/21554. 마법의 돌 장난감/마법의 돌 장난감.js
@@ -8,28 +8,33 @@ function findSpot(arr) {
 	let l = -1;
 	let r = -1;
 
-	outer: for (let i = 0; i < arr.length - 1; i++) {
+	const res = [];
+	for (let i = 0; i < arr.length - 1; i++) {
 		if (arr[i] > arr[i + 1]) {
 			l = i;
-
 			for (let j = i + 1; j < arr.length; j++) {
 				if (arr[j] < arr[i]) {
 					r = j;
 				} else {
-					break outer;
+					break;
 				}
+			}
+
+			if (l !== -1 && r !== -1) {
+				res.push([l, r]);
+				i = r + 1;
+				l = r = -1;
 			}
 		}
 	}
 
-	return [l, r];
+	return res;
 }
 function manipulate(arr, l, r) {
 	const replicated_arr = arr.slice(l, r + 1);
-
-	for (let i = l; i <= r; i++) {
-		arr[i] = replicated_arr[replicated_arr.length - 1 - i + l];
-	}
+	replicated_arr.reverse();
+	arr.splice(l, r - l + 1, ...replicated_arr);
+	manipulate.count++;
 }
 
 manipulate.count = 0;
@@ -42,14 +47,16 @@ function solution() {
 	const arr = input[idx++].split(' ').map(Number);
 	const orders = [];
 
-	while (manipulate.count < 100) {
-		const [l, r] = findSpot(arr);
+	for (let i = 0; i < N; i++) {
+		if (arr[i] === i + 1) continue;
+		if (manipulate.count > 100) break;
 
-		if (l === -1) break;
-		orders.push([l + 1, r + 1]);
-		manipulate(arr, l, r);
-		manipulate.count++;
+		const right_position = arr.indexOf(i + 1);
+		orders.push([i + 1, right_position + 1]);
+		manipulate(arr, i, right_position);
 	}
+
+	if (manipulate.count >= 101) return -1;
 
 	answer.push(manipulate.count);
 	answer.push(...orders);
