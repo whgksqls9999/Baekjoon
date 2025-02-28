c1f51895f97e8ecca607ea1614b5e0cf381f58ed
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2025-03-01 01:48:07 +0900

    [Silver IV] Title: 황금 칵테일, Time: 104 ms, Memory: 11668 KB -BaekjoonHub

---
+++ 백준/Silver/26596. 황금 칵테일/황금 칵테일.js
@@ -0,0 +1,28 @@
+const input = require('fs')
+	.readFileSync(0)
+	.toString()
+	.trim()
+	.split('\n');
+
+function solution() {
+	let idx = 0;
+	const N = Number(input[idx++]);
+	const map = new Map();
+	for (let i = 0; i < N; i++) {
+		const [prod, amt] = input[idx++].split(' ');
+		map.set(prod, (map.get(prod) ?? 0) + Number(amt));
+	}
+
+	const arr = Array.from(map).sort((a, b) => a[1] - b[1]);
+
+	const NUM = 1.618;
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (Math.floor(arr[i][1] * NUM) === arr[j][1]) return 'Delicious!';
+		}
+	}
+
+	return 'Not Delicious...';
+}
+
+console.log(solution());
