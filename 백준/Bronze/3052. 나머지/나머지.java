2666bb46620ccbc95a7f5b2b73236cec677f10fa
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2025-11-09 20:48:52 +0900

    [Bronze II] Title: 나머지, Time: 96 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,26 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        int[] array = new int[42];
 
-		int[] arr = new int[42];
-		for (int i = 0; i < 10; i++) {
-			++arr[Integer.parseInt(br.readLine()) % 42];
-		}
-		int cnt = 0;
-		for (int i : arr) {
-			if (i >= 1) {
-				++cnt;
-			}
-		}
-		System.out.println(cnt);
+        for (int i = 0; i < 10; ++i){
+            int input = Integer.parseInt(br.readLine());
+            ++array[input % 42];
+        }
 
-	}// main
+        int result = 0;
+        for (int i : array){
+            if (i >= 1){
+                ++result;
+            }
+        }
 
+        System.out.print(result);
+    }
 }
-// class
