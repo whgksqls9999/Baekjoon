b8fcff2fd3111f1bfa2424244846b5c0d49df72b
Jo Hanbin <85666312+whgksqls9999@users.noreply.github.com>
2025-11-15 00:59:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 51340 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,66 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static class Props {
-		int weight, value;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        StringTokenizer st = new StringTokenizer((br.readLine()));
 
-		public Props(int weight, int value) {
-			super();
-			this.weight = weight;
-			this.value = value;
-		}
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-	}
+        Item[] items = new Item[N];
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+            items[i] = new Item(W,V);
+        }
 
-		int N = Integer.parseInt(st.nextToken()); // 물품의 수
-		int K = Integer.parseInt(st.nextToken()); // 준서가 버틸 수 있는 무게
+        int[][] dp = new int[N+1][K+1];
 
-		Props[] props = new Props[N];
+        for (int i = 1; i <= N; i++){
+            Item item = items[i-1];
 
-		int maxWeight = 0; // dp 배열 만들기 위한 변수 maxWeight
+            for (int j = 1; j <= K; j++){
+                if (j < item.weight){
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j-item.weight] + item.value,dp[i-1][j]);
+                }
+            }
+        }
 
-		// 객체 배열에 물건들 담아주기
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			props[i] = new Props(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-			if (props[i].weight > maxWeight) {
-				maxWeight = props[i].weight;
-			}
-		}
+        System.out.print(dp[N][K]);
+    }
 
-		// dp 배열 생성
-		int[][] dp = new int[N + 1][K + 1];
-		int max = 0;
+    private static class Item {
+        int weight;
+        int value;
 
-		for (int i = 1; i < dp.length; i++) {
-			for (int j = 0; j < dp[i].length; j++) {
-				if (j < props[i - 1].weight) {
-					dp[i][j] = dp[i - 1][j];
-					if (dp[i][j] > max) {
-						max = dp[i][j];
-					}
-				} else {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - props[i - 1].weight] + props[i - 1].value);
-					if (dp[i][j] > max) {
-						max = dp[i][j];
-					}
-				}
-			}
-		}
+        public Item(int weight, int value){
+            this.weight = weight;
+            this.value = value;
+        }
 
-//		for (int i = 0; i < dp.length; i++) {
-//			System.out.println(Arrays.toString(dp[i]));
-//		}
 
-		System.out.println(max);
-	}
+    }
 }
+
